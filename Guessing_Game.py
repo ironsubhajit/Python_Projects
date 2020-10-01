a3e1a3a5ae40356fc9032cbac37849d1e7eebb2f
Subhajit <mandalsubhajit330@gmail.com>
2020-10-01 12:40:41 +0530

    create end-game function

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -23,16 +23,16 @@ def game_level(level=4):
 def start_game(val, guess):
     if val <= 4:
         print("\nOK all set,Lets start :D\n")
-        i=1
+        i = 1
         while i <= val:
             g = int(input("guess plz: "))
             if g==guess:
                 print("\ncongrates! you win! :D\n")
                 print("\nSir,You have really a good guessing skill.\n")
-                l=int(input("\npress 1. New Game 0. exit\n"))
-                if l==1:
+                l = end_game()
+                if l == 1:
                     break
-                elif l==0:
+                elif l == 0:
                     exit(1)
             else:
                 t=val-i
@@ -44,15 +44,23 @@ def start_game(val, guess):
         else:
             print("sorry,you are failed!\n")
             print(f"The answer is {guess}\n\nBetter luck next time.\n")
-            l=int(input("press 1. New 1Game 0. exit\n"))
+            l = end_game()
     else :
         print("\nYou can't guess more than 4 times\n")
-        l=int(input("\npress 1. continue 0. exit\n"))
+        l = end_game()
     return l
 
 
-l=int(input("\n1.New Game\n0.Quit\n"))
-while l==1:
+def end_game():
+    l = int(input("press 1. New 1Game 0. exit\n"))
+    if l == 1:
+        return 1
+    else:
+        return 0
+
+
+l = end_game()
+while l == 1:
     print("\nHello Sir,Welcome to guessing a Number."
           "\n\nLets see how strong your guessing skills are :D"
           "\nRules are so simple:"
@@ -61,5 +69,6 @@ while l==1:
     level = int(input("Enter Game Level(max is 4): "))
     val = game_level(level)
     l = start_game(val, guess)
-if l==0:
-    exit(1)
+if l == 0:
+    print("Thank You!")
+    exit(0)
