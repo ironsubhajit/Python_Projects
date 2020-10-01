8ccd262d93cb03800561db4cc8bc0d3267b453e9
Subhajit <mandalsubhajit330@gmail.com>
2020-10-01 12:30:10 +0530

    create start_game function

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -19,20 +19,13 @@ def game_level(level=4):
 
     return val
 
-l=int(input("\n1.New Game\n0.Quit\n"))
-while l==1:
-    print("\nHello Sir,Welcome to guessing a Number."
-          "\n\nLets see how strong your guessing skills are :D"
-          "\nRules are so simple:"
-          "\nyou have max of 4 chances to guess a no. which is between 0 to 9\n")
-    guess = rand.randint(0,9)
-    level = int(input("Enter Game Level(max is 4): "))
-    val = game_level(level)
-    if val<=4:
+
+def start_game(val, guess):
+    if val <= 4:
         print("\nOK all set,Lets start :D\n")
         i=1
-        while i<=val:
-            g=int(input("guess plz: "))
+        while i <= val:
+            g = int(input("guess plz: "))
             if g==guess:
                 print("\ncongrates! you win! :D\n")
                 print("\nSir,You have really a good guessing skill.\n")
@@ -55,5 +48,18 @@ while l==1:
     else :
         print("\nYou can't guess more than 4 times\n")
         l=int(input("\npress 1. continue 0. exit\n"))
+    return l
+
+
+l=int(input("\n1.New Game\n0.Quit\n"))
+while l==1:
+    print("\nHello Sir,Welcome to guessing a Number."
+          "\n\nLets see how strong your guessing skills are :D"
+          "\nRules are so simple:"
+          "\nyou have max of 4 chances to guess a no. which is between 0 to 9\n")
+    guess = rand.randint(0,9)
+    level = int(input("Enter Game Level(max is 4): "))
+    val = game_level(level)
+    l = start_game(val, guess)
 if l==0:
     exit(1)
